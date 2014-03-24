696cde0013a3adb2584f7512092018836442f73f
pleasereboot <yannick.yyy@gmail.com>
2014-03-24 09:53:06 -0400

    added modules + tpl edit
    flexslider
    admin language
    menu node
    menu node views

--- sites/default/themes/basic/templates/page.tpl.php
+++ sites/default/themes/basic/templates/page.tpl.php
@@ -1,117 +1,92 @@
 <div id="page" class="<?php print $classes; ?>"<?php print $attributes; ?>>
 
   <!-- ______________________ HEADER _______________________ -->
-
-  <header id="header">
-
-    <?php if ($logo): ?>
-      <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo">
-        <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>"/>
-      </a>
-    <?php endif; ?>
-
-    <?php if ($site_name || $site_slogan): ?>
-      <div id="name-and-slogan">
-
-        <?php if ($site_name): ?>
-          <?php if ($title): ?>
-            <div id="site-name">
-              <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><?php print $site_name; ?></a>
-            </div>
-          <?php else: /* Use h1 when the content title is empty */ ?>
-            <h1 id="site-name">
-              <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><?php print $site_name; ?></a>
-            </h1>
-          <?php endif; ?>
+  <div id="header-wrapper">
+    <header id="header">
+      <div class="menu-wrapper">
+        <?php if ($logo): ?>
+          <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo">
+            <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>"/>
+          </a>
         <?php endif; ?>
 
-        <?php if ($site_slogan): ?>
-          <div id="site-slogan"><?php print $site_slogan; ?></div>
-        <?php endif; ?>
-
+        <?php print theme('links', array('links' => menu_navigation_links('main-menu'), 'attributes' => array('id'=> array('main-menu'), 'class'=> array('menu', 'main-menu'))));  ?>
       </div>
-    <?php endif; ?>
 
-    <?php if ($page['header']): ?>
-      <div id="header-region">
-        <?php print render($page['header']); ?>
-      </div>
-    <?php endif; ?>
+      <?php if ($page['header']): ?>
+        <div id="header-region">
+          <?php print render($page['header']); ?>
+        </div>
+      <?php endif; ?>
 
-  </header> <!-- /header -->
-
-  <?php if ($main_menu || $secondary_menu): ?>
-    <nav id="navigation" class="menu <?php if (!empty($main_menu)) {print "with-primary";}
-      if (!empty($secondary_menu)) {print " with-secondary";} ?>">
-      <?php print theme('links', array('links' => $main_menu, 'attributes' => array('id' => 'primary', 'class' => array('links', 'clearfix', 'main-menu')))); ?>
-      <?php print theme('links', array('links' => $secondary_menu, 'attributes' => array('id' => 'secondary', 'class' => array('links', 'clearfix', 'sub-menu')))); ?>
-    </nav> <!-- /navigation -->
-  <?php endif; ?>
+    </header> <!-- /header -->
+  </div>
 
   <!-- ______________________ MAIN _______________________ -->
+  <div id="main-wrapper">
+    <div id="main" class="clearfix">
+      <section id="content">
 
-  <div id="main" class="clearfix">
+          <?php if ($breadcrumb || $title|| $messages || $tabs || $action_links): ?>
+            <div id="content-header">
 
-    <section id="content">
+              <?php //print $breadcrumb; ?>
 
-        <?php if ($breadcrumb || $title|| $messages || $tabs || $action_links): ?>
-          <div id="content-header">
+              <?php if ($page['highlighted']): ?>
+                <div id="highlighted"><?php print render($page['highlighted']) ?></div>
+              <?php endif; ?>
 
-            <?php print $breadcrumb; ?>
+              <?php print render($title_prefix); ?>
 
-            <?php if ($page['highlighted']): ?>
-              <div id="highlighted"><?php print render($page['highlighted']) ?></div>
-            <?php endif; ?>
+              <?php if ($title && !$is_front): ?>
+                <h1 class="title"><?php print $title; ?></h1>
+              <?php endif; ?>
 
-            <?php print render($title_prefix); ?>
+              <?php print render($title_suffix); ?>
+              <?php print $messages; ?>
+              <?php print render($page['help']); ?>
 
-            <?php if ($title): ?>
-              <h1 class="title"><?php print $title; ?></h1>
-            <?php endif; ?>
+              <?php if ($tabs): ?>
+                <div class="tabs"><?php print render($tabs); ?></div>
+              <?php endif; ?>
 
-            <?php print render($title_suffix); ?>
-            <?php print $messages; ?>
-            <?php print render($page['help']); ?>
+              <?php if ($action_links): ?>
+                <ul class="action-links"><?php print render($action_links); ?></ul>
+              <?php endif; ?>
 
-            <?php if ($tabs): ?>
-              <div class="tabs"><?php print render($tabs); ?></div>
-            <?php endif; ?>
+            </div> <!-- /#content-header -->
+          <?php endif; ?>
 
-            <?php if ($action_links): ?>
-              <ul class="action-links"><?php print render($action_links); ?></ul>
-            <?php endif; ?>
+          <div id="content-area">
+            <?php print render($page['content']) ?>
+          </div>
 
-          </div> <!-- /#content-header -->
-        <?php endif; ?>
+          <?php print $feed_icons; ?>
 
-        <div id="content-area">
-          <?php print render($page['content']) ?>
-        </div>
+      </section> <!-- /content-inner /content -->
 
-        <?php print $feed_icons; ?>
+      <?php if ($page['sidebar_first']): ?>
+        <aside id="sidebar-first" class="column sidebar first">
+          <?php print render($page['sidebar_first']); ?>
+        </aside>
+      <?php endif; ?> <!-- /sidebar-first -->
+      
+      <?php if ($page['sidebar_second']): ?>
+        <aside id="sidebar-second" class="column sidebar second">
+          <?php print render($page['sidebar_second']); ?>
+        </aside>
+      <?php endif; ?> <!-- /sidebar-second -->
 
-    </section> <!-- /content-inner /content -->
-
-    <?php if ($page['sidebar_first']): ?>
-      <aside id="sidebar-first" class="column sidebar first">
-        <?php print render($page['sidebar_first']); ?>
-      </aside>
-    <?php endif; ?> <!-- /sidebar-first -->
-    
-    <?php if ($page['sidebar_second']): ?>
-      <aside id="sidebar-second" class="column sidebar second">
-        <?php print render($page['sidebar_second']); ?>
-      </aside>
-    <?php endif; ?> <!-- /sidebar-second -->
-
-  </div> <!-- /main -->
+    </div> <!-- /main -->
+  </div>
 
   <!-- ______________________ FOOTER _______________________ -->
 
-  <?php if ($page['footer']): ?>
-    <footer id="footer">
-      <?php print render($page['footer']); ?>
-    </footer> <!-- /footer -->
-  <?php endif; ?>
-
+  <div id="footer-wrapper">
+    <?php if ($page['footer']): ?>
+      <footer id="footer">
+        <?php print render($page['footer']); ?>
+      </footer> <!-- /footer -->
+    <?php endif; ?>
+  </div>
 </div> <!-- /page -->
